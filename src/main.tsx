e12bcba5b48e10c6d107b067dd27fbc220b10f21
NaijaB0T <femivideograph@gmail.com>
2025-03-13 22:15:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,27 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import React from 'react';
+import { hydrateRoot, createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+const container = document.getElementById('root');
+
+if (container) {
+  if (container.innerHTML.trim().length) {
+    // If we have content in the container, we're doing hydration
+    hydrateRoot(
+      container,
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  } else {
+    // Normal client-side rendering
+    createRoot(container).render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  }
+}
